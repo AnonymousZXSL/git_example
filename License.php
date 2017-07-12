4c1d15c3fb1b6919c94786f01482234d96d852f6
commanderZX <anonymous.net@list.ru>
2017-07-12 16:30:42 +0400

    add All

--- License.php
+++ License.php
@@ -1,8 +1,15 @@
-sadsadsad
-123123123
-
-sddsadasd
-sddsadasd
-sddsadasd
-sddsaDdasd
-sddsaDdasd
+<?php
+    $id = $_SESSION['userID'];
+    $myMessages = $DataBase -> query("SELECT `send`, `time` FROM message WHERE `userID` = $id");
+    while($myM = mysqli_fetch_array($myMessages)){
+        echo "<p>" . "<span>". $myM['send'] .  "</span>" . " / " . $myM['time'] . "</p><hr>";
+    }
+?>
+</div>
+<div class="sendInboxDiv">
+<?php
+$inboxMessages = $DataBase -> query("SELECT `inbox`, `time` FROM messageinbox WHERE `userID`=$id");
+while($myInb = mysqli_fetch_array($inboxMessages)){
+    echo "<p>" . $myInb['inbox'] . " / " . $myInb['time'] . "</p><hr>";
+}
+?>
